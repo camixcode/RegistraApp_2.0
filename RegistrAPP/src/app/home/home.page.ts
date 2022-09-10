733bc6e4ae40389235abe0f53c6ce010f88e72b9
psicodelicko <101906675+psicodelicko@users.noreply.github.com>
2022-09-09 23:36:35 -0400

    coomit

--- RegistrAPP/src/app/home/home.page.ts
+++ RegistrAPP/src/app/home/home.page.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { AlertController } from '@ionic/angular';
+import { Usuario } from '../app.component';
+
 
 @Component({
   selector: 'app-home',
@@ -7,6 +11,53 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  constructor(
+    private alertController: AlertController,
+    private activatedRoute: ActivatedRoute
+    ) {}
+    
+    usuario = new Usuario(1,"","","","","","","");
 
+
+    async presentAlert() {
+      if(this.usuario.nombre.length<=0){
+        const alert = await this.alertController.create({
+          subHeader: 'Usuario',
+          message: 'Error campos invalidos',
+          buttons: ['OK'],
+        });
+    
+        await alert.present();
+      }else{
+        const alert = await this.alertController.create({
+          subHeader: 'Usuario',
+          message: 'Su nombre es '+this.usuario.nombre+" "+this.usuario.apellido,
+          buttons: ['OK'],
+        });
+    
+        await alert.present();
+      }
+    }
+  
+
+  limpiar(){
+    this.usuario.nombre = ""
+
+  }
+
+  async guardar(){
+    localStorage.setItem('usuario', JSON.stringify(this.usuario));
+    const alert = await this.alertController.create({
+      subHeader: 'Usuario',
+      message: 'cuenta creada exitosamente',
+      buttons: ['OK'],
+    });
+
+    await alert.present();
+  }
+
+  ngOnInit() {
+    this.usuario.nombreUsuario=this.activatedRoute.snapshot.paramMap.get("nombreUsuario");
+    console.log(this.usuario.nombreUsuario)
+  }
 }
--- RegistrAPP/src/app/login/login.page.ts
+++ RegistrAPP/src/app/login/login.page.ts
@@ -9,13 +9,10 @@ import { Usuario } from '../app.component';
 export class LoginPage implements OnInit {
 
   constructor() { }
-  usuario = new Usuario(1,"","","","","","");
+  usuario = new Usuario(1,"","","","","","","");
 
   ngOnInit() {
   }
-  ingresar(){
-    console.log(this.usuario.nombreUsuario)
-    localStorage.setItem('usuario', JSON.stringify(this.usuario))
-  }
+  
 
 }
