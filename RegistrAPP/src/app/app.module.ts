df6de9d041ebe8c1d0e0b5043b4470bbae4526c5
AlexD30 <yer.vasquez@duocuc.cl>
2022-09-09 22:40:01 -0300

    Perfil

--- RegistrAPP/src/app/app.module.ts
+++ RegistrAPP/src/app/app.module.ts
@@ -6,10 +6,19 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import {MatIconModule} from '@angular/material/icon';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [
+  BrowserModule, 
+  IonicModule.forRoot(), 
+  AppRoutingModule, 
+  BrowserAnimationsModule,
+  MatIconModule
+  ],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
 })
--- RegistrAPP/src/app/login/login.module.ts
+++ RegistrAPP/src/app/login/login.module.ts
@@ -7,13 +7,15 @@ import { IonicModule } from '@ionic/angular';
 import { LoginPageRoutingModule } from './login-routing.module';
 
 import { LoginPage } from './login.page';
+import {MatIconModule} from '@angular/material/icon';
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
-    LoginPageRoutingModule
+    LoginPageRoutingModule,
+    MatIconModule
   ],
   declarations: [LoginPage]
 })
